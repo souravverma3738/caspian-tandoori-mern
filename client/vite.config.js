728735dcfd6828263f83ba242c0129a3df2ab321
Administrator <souravverma1582000@gmail.com>
2026-05-25 00:35:23 +0100

    change the price in the menu

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,34 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Caspian Tandoori',
-        short_name: 'Caspian',
-        description: 'Restaurant Ordering System',
-        theme_color: '#000000',
-        background_color: '#000000',
-        display: 'standalone',
-        start_url: '/admin-login',
-        scope: '/',
-        icons: [
-          {
-            src: '/caspian_logo.jpg',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/caspian_logo.jpg',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
   ]
 })
